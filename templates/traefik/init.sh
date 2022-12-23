0df641bf26fbb7e6eb8a7cb8916f28d5baeaea15
Ryan Fogle <ryanfogle98@gmail.com>
2022-12-23 17:24:19 -0600

    traefik configuration
    Signed-off-by: Ryan Fogle <ryanfogle98@gmail.com>

--- templates/traefik/init.sh
+++ templates/traefik/init.sh
@@ -7,18 +7,24 @@ cd "$(dirname "$0")"
 export $(grep -v '^#' .env | xargs -d '\n')
 export $(grep -v '^#' ../../.env | xargs -d '\n')
 
+# create config directory on your machine
+mkdir -p ${DATA_LOCATION}/${SERVICE}/config
+
+# copy over custom files
+cp -r ./config/custom ${DATA_LOCATION}/${SERVICE}/custom
+
+# create amce file
+touch ${DATA_LOCATION}/${SERVICE}/config/acme.json
+chmod 600 ${DATA_LOCATION}/${SERVICE}/config/acme.json
+
 # create files
 # https://stackoverflow.com/questions/14155596/how-to-substitute-shell-variables-in-complex-text-files
-envsubst < config/config.sample.yml > config/config.yml
-envsubst < config/traefik.sample.yml > config/traefik.yml
+envsubst < config/config.sample.yml > ${DATA_LOCATION}/${SERVICE}/config/config.yml
+envsubst < config/traefik.sample.yml > ${DATA_LOCATION}/${SERVICE}/config/traefik.yml
 
 # setup password 
 export USERPASS=$(echo $(htpasswd -nb admin $PASSWORD_TRAEFIK) | sed -e s/\\$/\\$\\$/g)
 echo $USERPASS
-
-# create config directory on your machine
-mkdir -p ${DATA_LOCATION}/${SERVICE}/config
-cp ./config ${DATA_LOCATION}/${SERVICE}
 
 # compile container
 docker compose config > deploy.yml
